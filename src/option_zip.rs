f209dc998ec5e9d6ed2f239880084790925a8297
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2019-12-21 04:32:53 -0300

    Macros for trait implementations
    Adds macros for zip function generation and add zip functions for more arguments
    
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- src/option_zip.rs
+++ src/option_zip.rs
@@ -1,13 +1,36 @@
-pub trait OptionZip {
-    fn zip<T, U>(v1: Option<T>, v2: Option<U>) -> Option<(T, U)> {
-        match v1 {
-            Some(t) => match v2 {
-                Some(u) => Some((t, u)),
-                None => None,
-            },
-            None => None,
+macro_rules! zip_fn {
+    ($f:ident, [ $($arg:ident: $type:ident), * ]) => {
+        fn $f<$($type,)*>($($arg: core::option::Option<$type>,)*)
+                          -> core::option::Option<($($type,)*)> {
+            match_args!($($arg),* => $($arg),*)
         }
-    }
+    };
+}
+
+macro_rules! match_args {
+    ($opt:ident => $($arg:ident),*) => {
+        match $opt {
+            core::option::Option::Some($opt) => core::option::Option::Some(($($arg),*)),
+            core::option::Option::None => core::option::Option::None
+        }
+    };
+
+    ($opt:ident, $($opts:ident), + => $($arg:ident),*) => {
+        match $opt {
+            core::option::Option::Some($opt) => match_args!($($opts),* => $($arg),*),
+            core::option::Option::None => core::option::Option::None,
+        }
+    };
+}
+
+pub trait OptionZip {
+    zip_fn!(zip, [v1: T1, v2: T2]);
+
+    zip_fn!(zip3, [v1: T1, v2: T2, v3: T3]);
+
+    zip_fn!(zip4, [v1: T1, v2: T2, v3: T3, v4: T4]);
+
+    zip_fn!(zip5, [v1: T1, v2: T2, v3: T3, v4: T4, v5: T5]);
 }
 
 #[doc(hidden)]
--- src/result_zip.rs
+++ src/result_zip.rs
@@ -1,13 +1,36 @@
-pub trait ResultZip {
-    fn zip<T, U, E>(v1: Result<T, E>, v2: Result<U, E>) -> Result<(T, U), E> {
-        match v1 {
-            Ok(t) => match v2 {
-                Ok(u) => Ok((t, u)),
-                Err(e) => Err(e),
-            },
-            Err(e) => Err(e),
+macro_rules! zip_fn {
+    ($f:ident, [ $($arg:ident: $type:ident), * ]) => {
+        fn $f<$($type,)* E>($($arg: core::result::Result<$type, E>,)*)
+                             -> core::result::Result<($($type,)*), E> {
+            match_args!($($arg),* => $($arg),*)
         }
-    }
+    };
+}
+
+macro_rules! match_args {
+    ($opt:ident => $($arg:ident),*) => {
+        match $opt {
+            core::result::Result::Ok($opt) => core::result::Result::Ok(($($arg),*)),
+            core::result::Result::Err(e) => core::result::Result::Err(e)
+        }
+    };
+
+    ($opt:ident, $($opts:ident), + => $($arg:ident),*) => {
+        match $opt {
+            core::result::Result::Ok($opt) => match_args!($($opts),* => $($arg),*),
+            core::result::Result::Err(e) => core::result::Result::Err(e),
+        }
+    };
+}
+
+pub trait ResultZip {
+    zip_fn!(zip, [v1: T1, v2: T2]);
+
+    zip_fn!(zip3, [v1: T1, v2: T2, v3: T3]);
+
+    zip_fn!(zip4, [v1: T1, v2: T2, v3: T3, v4: T4]);
+
+    zip_fn!(zip5, [v1: T1, v2: T2, v3: T3, v4: T4, v5: T5]);
 }
 
 #[doc(hidden)]

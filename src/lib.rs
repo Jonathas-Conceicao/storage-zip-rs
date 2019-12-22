7a4fc8b5170d922c20fbbbc601cd368a398e6ffa
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2019-12-22 19:17:46 -0300

    Add tests with zip of different types
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,16 @@ fn option_zip() {
 }
 
 #[test]
+fn option_zip_with_different_types() {
+    let option_value_one = Some(0);
+    let option_value_two = Some(5.);
+    let option_value_three = Some("Three");
+
+    let zipped_options = Option::zip3(option_value_one, option_value_two, option_value_three);
+    assert_eq!(zipped_options, Some((0, 5., "Three")));
+}
+
+#[test]
 fn option_zip_with_none() {
     let option_value_one = Some(0);
     let option_value_two: Option<i32> = None;
@@ -37,6 +47,16 @@ fn result_zip() {
 
     let zipped_options = Result::zip(result_value_one, result_value_two);
     assert_eq!(zipped_options, Ok((0, 5)));
+}
+
+#[test]
+fn result_zip_with_different_types() {
+    let result_value_one = Result::<i32, ()>::Ok(0);
+    let result_value_two = Result::<f32, ()>::Ok(5.);
+    let result_value_three = Result::<&str, ()>::Ok("three");
+
+    let zipped_options = Result::zip3(result_value_one, result_value_two, result_value_three);
+    assert_eq!(zipped_options, Ok((0, 5., "three")));
 }
 
 #[test]
